4942e640a92b32f9ef3ed11540bce928cd813d94
X00011999 <thomas.mayer@smail.auswaertiges-amt.de>
2021-05-13 20:22:33 +0200

    complete API keys

--- src/server/api_data.js
+++ src/server/api_data.js
@@ -1,17 +1,17 @@
 const weatherbitKey = function() {
-    const key = "40ae74205aa54f87a9cb...";
+    const key = "40ae74205aa54f87a9cb923a8ab80c34";
 
     return key;
 }
 
 const pixabayKey = function() {
-    const key = "21396302-acfd49eefb4f7...";
+    const key = "21396302-acfd49eefb4f7e2f61625c5c1";
 
     return key;
 }
 
 const geoNamesUserName = function() {
-    const uname = "demo";
+    const uname = "tnmayer";
 
     return uname;
 }
